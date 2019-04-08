2ff07d8d2dba4e24319c64fd9d368b7ff2e8d3bb
Paul Craig <paul_craig_16@hotmail.com>
2019-04-08 00:42:28 -0400

    Use smaller favicon file
    The login page is ~6KB to download whereas the favicon file was ~15KB.
    
    Can you believe?
    
    This commit adds a 1KB 32x32 png favicon file and changes the
    reference in the site's <head>.
    
    It also deletes the old one, which is fine since this is more or
    less a placeholder anyway.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,7 @@ const document = ({ title, locale, content }) => {
         ${metaIfSHA() || ''}
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon" sizes="32x32"  />
         <title>${title}</title>
         <style>
           * {
