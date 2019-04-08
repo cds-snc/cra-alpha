425418ed8082611e1a11b27c5c6d211da408f281
Paul Craig <paul_craig_16@hotmail.com>
2019-04-08 15:53:32 -0400

    Add unit tests for the Dashboard page component
    Pretty simple, just checking for a few hardcoded elements, and
    that values we pass in appear on the page.

---
+++ src/pages/__tests__/Document.test.js
@@ -0,0 +1,60 @@
+const render = require('preact-render-to-string')
+const cheerio = require('cheerio')
+const { html } = require('../../utils.js')
+
+const Dashboard = require('../Dashboard.js')
+
+describe('<Dashboard>', () => {
+  const data = {
+    name: 'Fred Smith',
+    sin: '123-456-789',
+    dobDay: '18',
+    dobMonth: '06',
+    dobYear: '1971',
+    address: 'Mississauga',
+  }
+
+  const expectedStrings = [
+    'Fred Smith',
+    '123-456-789',
+    '18-06-1971',
+    'Mississauga',
+  ]
+
+  test('renders h1 as expected', () => {
+    const $ = cheerio.load(
+      render(
+        html`
+          <${Dashboard} data=${data} />
+        `,
+      ),
+    )
+    expect($('h1').length).toBe(1)
+    expect($('h1').text()).toEqual('Dashboard')
+
+    expect($('button').length).toBe(1)
+    expect($('button').text()).toEqual('Submit taxes')
+
+    expect($('a[href="/logout"]').length).toBe(1)
+    expect($('a[href="/logout"]').text()).toEqual('Log out')
+  })
+
+  expectedStrings.map(str => {
+    test(`renders ${str} on page`, () => {
+      const $ = cheerio.load(
+        render(
+          html`
+            <${Dashboard} data=${data} />
+          `,
+        ),
+      )
+      expect($('h1').text()).toEqual('Dashboard')
+
+      expect(
+        $('h1')
+          .next()
+          .html(),
+      ).toContain(str)
+    })
+  })
+})
