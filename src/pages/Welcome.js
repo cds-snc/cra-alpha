d73dc6f28ff932d9ebf9f2475631a29827789c0e
Paul Craig <paul_craig_16@hotmail.com>
2019-04-12 14:06:29 -0400

    Mario

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -14,7 +14,7 @@ const Welcome = ({ locale }) =>
   html`
     <${Layout}>
       <div class=${welcome}>
-        <h1>Claim tax benefits (CTB)</h1>
+        <h1>Claim tax benefits(CTB)</h1>
         <p>${polyglot.t(`${locale}.welcome`)}.</p>
 
         <p>You will need to know:</p>
@@ -22,6 +22,7 @@ const Welcome = ({ locale }) =>
           <li>Your first name</li>
         </ul>
 
+        <p>Hi Mario.</p>
         <p>
           Then, you will see the information CRA has on file about you. If it is
           up to date, you will be able to file your return immediately.
