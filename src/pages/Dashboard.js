e3cc4713b61792c56eff057df84314c80703c049
Paul Craig <paul_craig_16@hotmail.com>
2019-04-14 18:38:25 -0400

    Added 2 more tables to dashboard page
    Busier, but, again, this seems to be the generic interface we're
    going for.
    
    Updated the test slightly to be more inclusive for where it looks
    for the row values to be printed, and to make sure the h2s exist.
    
    Also updated the prettier instructions to allow longer line lengths
    otherwise, prettier and eslint clash overformatting.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -20,14 +20,19 @@ const dashboard = css`
 const submitButton = css`
   width: 200px;
 `
-const makeRows = ({ name, address, maritalStatus, children, income }) => {
+const aboutYouRows = ({ name, address }) => {
+  return [{ key: 'Name', value: name }, { key: 'Mailing address', value: address }]
+}
+
+const yourFamilyRows = ({ maritalStatus, children }) => {
   return [
-    { key: 'Name', value: name },
-    { key: 'Mailing address', value: address },
     { key: 'Marital status', value: maritalStatus },
     { key: 'Number of children', value: children },
-    { key: 'Annual income', value: income },
   ]
+}
+
+const yourIncomeRows = ({ income }) => {
+  return [{ key: 'Annual income', value: income }]
 }
 
 const Dashboard = ({ data = {}, errors = {} }) =>
@@ -42,9 +47,10 @@ const Dashboard = ({ data = {}, errors = {} }) =>
       <div class=${dashboard}>
         <${LogoutLink} />
         <h1>Dashboard</h1>
-        <div>
-          <${SummaryTable} rows=${makeRows(data)} ifEditable=${false} //>
-        </div>
+
+        <${SummaryTable} title="About you" rows=${aboutYouRows(data)} ifEditable=${false} //>
+        <${SummaryTable} title="Your family" rows=${yourFamilyRows(data)} ifEditable=${false} //>
+        <${SummaryTable} title="Your income" rows=${yourIncomeRows(data)} ifEditable=${false} //>
 
         <p>
           Once you have provided your consent, go ahead and submit. (<a
--- src/pages/__tests__/Dashboard.test.js
+++ src/pages/__tests__/Dashboard.test.js
@@ -15,6 +15,8 @@ describe('<Dashboard>', () => {
 
   const expectedStrings = Object.values(data)
 
+  const expectedH2s = ['About you', 'Your family', 'Your income']
+
   test('renders h1 as expected', () => {
     const $ = cheerio.load(
       render(
@@ -44,11 +46,24 @@ describe('<Dashboard>', () => {
       )
       expect($('h1').text()).toEqual('Dashboard')
 
+      expect($('dl').text()).toContain(str)
+    })
+  })
+
+  expectedH2s.map((str, i) => {
+    test(`renders h2 with "${str}"`, () => {
+      const $ = cheerio.load(
+        render(
+          html`
+            <${Dashboard} data=${data} />
+          `,
+        ),
+      )
       expect(
-        $('h1')
-          .next()
-          .html(),
-      ).toContain(str)
+        $('h2')
+          .eq(i)
+          .text(),
+      ).toEqual(str)
     })
   })
 })
