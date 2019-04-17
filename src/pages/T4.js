0f4ae7aaa6728f21555731bc7fa94add175386e3
Stéphane Boisvert <stboisvert@gmail.com>
2019-04-17 14:32:21 -0400

    Try to get income data from "API"
    This isn't working yet. Not sure what syntax I'm supposed to use.

--- src/pages/T4.js
+++ src/pages/T4.js
@@ -23,26 +23,23 @@ const submitButtonCSS = css`
 `
 //todo How to get t4 info? Where does it get injected from? user object passed with an array of t4s?
 //is this passed in to the Dashbord object which then passes it to summary table?
-const t4Data = ({
-  employerName,
-  year,
-  socialInsuranceNumber,
-  employmentIncome,
-  incomeTaxDeducted,
-}) => {
+const t4Data = ({ employerName, year, box12, box14, box22 }) => {
   return [
-    { key: 'Employer Name', value: 'Acme' },
-    { key: 'Year', value: 2019 },
-    { key: 'Box12', value: '123456789' },
-    { key: 'Box14', value: 50000 },
-    { key: 'Box22', value: 5000 },
+    { key: 'Employer Name', value: employerName },
+    { key: 'Year', value: year },
+    { key: 'Box12', value: box12 },
+    { key: 'Box14', value: box14 },
+    { key: 'Box22', value: box22 },
   ]
 }
 
 const T4 = ({ data = {}, errors = {} }) =>
   html`
     <${Layout}>
-
+      ${console.log('test1', data)}
+      ${console.log('test2', data.income)}
+      ${console.log('test3', t4Data(data))}
+      ${console.log('test4', t4Data(data.income))}
       ${Object.keys(errors).length > 0 &&
         html`
           <${ErrorList} errors=${errors} //>
@@ -54,9 +51,9 @@ const T4 = ({ data = {}, errors = {} }) =>
         <div>
         <img src='/t4.png' />
         </div>
-<${SummaryTable} title="Income Data" rows=${t4Data(data)} ifEditable=${false} //>
-<${ButtonLink} href="/confirmation" style=${submitButtonCSS}>Consent<//>
-<${ButtonLink} href="/confirmation" style=${submitButtonCSS}>Make Changes<//>
+<${SummaryTable} title="Income Data" rows=${t4Data(data.income)} ifEditable=${false} //>
+<${ButtonLink} href="/confirmation" style=${submitButtonCSS}>This information is accurate<//>
+<${ButtonLink} href="/confirmation" style=${submitButtonCSS}>I need to make changes<//>
       </div>
     </${Layout}>
   `
