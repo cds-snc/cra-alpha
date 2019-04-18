8db747ef638ef643e5e21f862ff92408fb35d49a
Stéphane Boisvert <stboisvert@gmail.com>
2019-04-18 13:00:06 -0400

    Clean up console.log and small style changes

--- src/pages/T4.js
+++ src/pages/T4.js
@@ -18,9 +18,16 @@ const t4CSS = css`
   }
 `
 
-const submitButtonCSS = css`
+const changesButtonCSS = css`
   width: 200px;
+  background-color: pink;
 `
+
+const consentButtonCSS = css`
+  width: 200px;
+  float: right;
+`
+
 //todo How to get t4 info? Where does it get injected from? user object passed with an array of t4s?
 //is this passed in to the Dashbord object which then passes it to summary table?
 const t4Data = ({ employerName, year, box12, box14, box22 }) => {
@@ -36,24 +43,16 @@ const t4Data = ({ employerName, year, box12, box14, box22 }) => {
 const T4 = ({ data = {}, errors = {} }) =>
   html`
     <${Layout}>
-      ${console.log('test1', data)}
-      ${console.log('test2', data.income)}
-      ${console.log('test3', t4Data(data))}
-      ${console.log('test4', t4Data(data.income))}
-      ${Object.keys(errors).length > 0 &&
-        html`
-          <${ErrorList} errors=${errors} //>
-        `}
-
       <div class=${t4CSS}>
         <${LogoutLink} />
         <h1>Income</h1>
         <div>
         <img src='/t4.png' />
         </div>
+
 <${SummaryTable} title="Income Data" rows=${t4Data(data.income)} ifEditable=${false} //>
-<${ButtonLink} href="/confirmation" style=${submitButtonCSS}>This information is accurate<//>
-<${ButtonLink} href="/confirmation" style=${submitButtonCSS}>I need to make changes<//>
+<${ButtonLink} href="/confirmation" style=${changesButtonCSS}>I need to make changes<//>
+<${ButtonLink} href="/confirmation" style=${consentButtonCSS}>This information is accurate<//>
       </div>
     </${Layout}>
   `
