c0f1ec9e4b2d4a101852f077eec6cfb154333e7f
Paul Craig <paul_craig_16@hotmail.com>
2019-04-18 00:57:08 -0400

    First test
    Just loads the home page but doesn't do anything else.

---
+++ cypress/integration/full_run_spec.js
@@ -0,0 +1,5 @@
+describe('Full run through', function() {
+  it('successfully loads the home page', function() {
+    cy.visit('/')
+  })
+})
--- cypress/integration/sample_spec.js
+++
@@ -1,7 +0,0 @@
-describe('My First Test', function() {
-  it('finds the content "type"', function() {
-    cy.visit('https://example.cypress.io')
-
-    cy.contains('type')
-  })
-})
