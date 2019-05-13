8573571f2f8a100504029d0d3f2ada36146ef264
Stéphane Boisvert <stephane.boisvert@cds-snc.ca>
2019-05-13 16:07:24 -0400

    Fix up checklist test

--- src/pages/__tests__/Checklist.test.js
+++ src/pages/__tests__/Checklist.test.js
@@ -5,21 +5,57 @@ const Checklist = require('../Checklist.js')
 
 describe('<CheckList>', () => {
   const data = {
-    name: 'Fred Smith',
-    address: '21275 Lakeshore Dr\nSainte-Anne-de-Bellevue\nQuébec\nH9X 3L9',
-    maritalStatus: 'Widowed',
-    children: '8',
-    income: {
-      employerName: 'BRAVO Corp',
-      year: 2019,
-      box12: '321-987-645',
-      box14: 10000,
-      box22: 1000,
+    _matches: ['john', 'j'],
+    personal: {
+      name: 'John Caldwell Abbott',
+      address: '21275 Lakeshore Dr\nSainte-Anne-de-Bellevue\nQuébec\nH9X 3L9',
+      maritalStatus: 'Widowed',
+      children: '8',
+      SIN: '123-456-789',
     },
+    return: {
+      // Income
+      line101: 12000, //Employment income (All t4s Box 14 added up)
+      line113: 1000, //Old Age Security Pension (OAS)
+      line146: 500, // Guaranteed income supplement (GIS)
+      line150: 12000, // total income
+      line236: 11000, // Net income
+      line260: 10000, // Taxable income
+      //Non-Refundable Tax Credits (NRTCs)
+      line350: 1200, //Total Federal NRTCs
+      line6150: 1000, //Total Provincial NRTCs
+      line300: 12500, //Basic Personal Amount
+      line303: 5000, //Spouce Common-Law Amount
+      line305: 0, // Amount for Eligible Dependants
+      line316: 0, //Disability (self)
+      line330: 0, // Medical for Spouse
+      line331: 0, //Medical for other dependants
+      line367: 0, // Caregiver for Infirm Children Amount
+      //Taxes
+      line420: 250, //Net Federal Tax
+      line428: 50, // Net Provincial Tax
+      line435: 300, // Total Payable
+      line437: 500, //Total Deducted (Calculated)
+      line482: 200, //Total Credits (Calculated)
+      line484: 200, //Refund (Calculated)
+      line485: 0, //Balance Owing (Calculated)
+    },
+    t4s: [
+      {
+        employerName: 'BRAVO Corp',
+        year: 2019,
+        box14: 10000, //Employment income
+        box22: 1000, // Income Tax Deducted
+        box10: 'ON', //Province of employment
+        box16: 100, //Employee CPP Contributions
+        box24: 10000, //EI Insurable earnings
+        box26: 10000, //CPP pensionable earnings
+      },
+    ],
   }
 
-  const expectedStrings = Object.values(data)
-  const expectedStringsIncome = Object.values(data.income)
+  const expectedStringspersonal = Object.values(data.personal)
+  const expectedStringsReturn = Object.values(data.return)
 
   test('renders h1 as expected', () => {
     const $ = cheerio.load(
@@ -39,7 +75,7 @@ describe('<CheckList>', () => {
     expect($('a[href="/logout"]').text()).toEqual('Log out')
   })
 
-  expectedStrings.slice(0,4).map(str => {
+  expectedStringspersonal.map(str => {
     test(`renders ${str} on page`, () => {
       const $ = cheerio.load(
         render(
@@ -53,7 +89,7 @@ describe('<CheckList>', () => {
     })
   })
 
-  expectedStringsIncome.map(str => {
+  expectedStringsReturn.map(str => {
     test(`renders ${str} on page`, () => {
       const $ = cheerio.load(
         render(
