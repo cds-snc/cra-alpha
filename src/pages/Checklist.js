c32e15a6c69f9e72a6a871af253d034f555454cb
nmakuch <makuch.nick@gmail.com>
2019-05-30 19:11:56 -0400

    review changes

--- src/pages/Checklist.js
+++ src/pages/Checklist.js
@@ -1,6 +1,6 @@
 const { css } = require('emotion')
 const { loggedInStyles, theme } = require('../styles.js')
-const { html } = require('../utils.js')
+const { html, currencyFormatter } = require('../utils.js')
 const Layout = require('../components/Layout.js')
 const Accordion = require('../components/Accordion.js')
 const LogoutLink = require('../components/LogoutLink.js')
@@ -20,11 +20,19 @@ const checklistH2 = css`
 
 const aboutYouRows = ({ firstName, lastName, address, maritalStatus, children, SIN }) => {
   return [
-    { key: 'Name', value: firstName + ' ' + lastName, id: 'name' },
-    { key: 'Mailing address', value: address, id: 'address' },
-    { key: 'Marital status', value: maritalStatus, id: 'maritalStatus' },
-    { key: 'Number of children', value: children, id: 'children' },
-    { key: 'Social Insurance Number (SIN)', value: SIN, id: 'sin' },
+<<<<<<< HEAD
+    { key: 'Name', value: firstName + ' ' + lastName },
+    { key: 'Mailing address', value: address },
+    { key: 'Marital status', value: maritalStatus },
+    { key: 'Number of children', value: children },
+    { key: 'Social Insurance Number (SIN)', value: SIN },
+=======
+    { key: 'Name', value: name },
+    { key: 'Mailing address', value: address },
+    { key: 'Marital status', value: maritalStatus },
+    { key: 'Number of children', value: children },
+    { key: 'Social Insurance Number (SIN)', value: SIN },
+>>>>>>> review changes
   ]
 }
 
@@ -39,14 +47,14 @@ const totalTaxRows = ({
   line482,
 } = {}) => {
   return [
-    { key: 'Basic personal amount (300)', value: line300, id: 'line300' },
-    { key: 'Spousal amount (303)', value: line303, id: 'line303' },
-    { key: 'Dependents amount (305)', value: line305, id: 'line305' },
+    { key: 'Basic personal amount (300)', value: line300 },
+    { key: 'Spousal amount (303)', value: line303 },
+    { key: 'Dependents amount (305)', value: line305 },
     { key: 'Caregiver amount (367)', value: line367, id: 'line367' },
-    { key: 'Disability amount (316)', value: line316, id: 'line316' },
-    { key: 'Medical Expenses (330)', value: line330, id: 'line330' },
+    { key: 'Disability amount (316)', value: line316 },
+    { key: 'Medical Expenses (330)', value: line330 },
     { key: 'Medical Expenses (331)', value: line331, id: 'line331' },
-    { key: 'Total', value: line482, id: 'line482' },
+    { key: 'Total', value: line482 },
   ]
 }
 
@@ -62,25 +70,35 @@ const refundRows = ({
   line484,
 } = {}) => {
   return [
-    { key: 'Total Income (150)', value: line150, id: 'line150' },
-    { key: 'Net Income (236)', value: line236, id: 'line236' },
+    { key: 'Total Income (150)', value: line150 },
+    { key: 'Net Income (236)', value: line236 },
     { key: 'Taxable Income (260)', value: line260, id: 'line260' },
-    { key: 'Net Federal Tax (420)', value: line420, id: 'line420' },
+    { key: 'Net Federal Tax (420)', value: line420 },
     { key: 'Net Ontario Tax (428)', value: line428, id: 'line428' },
-    { key: 'Total Payable (435)', value: line435, id: 'line435' },
-    { key: 'Total Deducted (437)', value: line437, id: 'line437' },
+    { key: 'Total Payable (435)', value: line435 },
+    { key: 'Total Deducted (437)', value: line437 },
     { key: 'Total Credits (482)', value: line482, id: 'line482' },
-    { key: 'Total payable minus credits', value: '400', id: 'totalPayableMinus' },
-    { key: 'Previous balance owed', value: '0', id: 'previousBalance' },
-    { key: 'Current balance owed', value: '0', id: 'currentBalance' },
-    { key: 'Total Refund (484)', value: line484, id: 'line484' },
+    { key: 'Total payable minus credits', value: '$400.00' },
+    { key: 'Previous balance owed', value: '$0.00' },
+    { key: 'Current balance owed', value: '$0.00', id: 'currentBalance' },
+    { key: 'Total Refund (484)', value: line484 },
   ]
+}
+
+var formattedUser = {}
+
+function myFunction(data) {
+  Object.entries(data).map(entry => {
+    var entryKey = entry[0]
+    var entryValue = entry[1]
+    formattedUser[entryKey] = currencyFormatter.format(entryValue)})
 }
 
 const Checklist = ({ user = {}, locale }) =>
   html`
     <${Layout}>
       <div class=${loggedInStyles}>
+      ${myFunction(user.return)}
         <${LogoutLink} />
         <h1>${polyglot.t(`${locale}.checklist.title`)}</h1>
         <p>
@@ -88,27 +106,27 @@ const Checklist = ({ user = {}, locale }) =>
         </p>
 
         <${Accordion} header="${polyglot.t(`${locale}.checklist.personalInformation`)}">
-          <${SummaryTable} keyBold=${true} rows=${aboutYouRows(user.personal)} />
+          <${SummaryTable} rows=${aboutYouRows(user.personal)} />
         <//>
 
         <h2 class=${checklistH2}>${polyglot.t(`${locale}.checklist.financialInformation`)}</h2>
 
         <dl>
-          <${SummaryRow} keyBold=${true} currency=${true} value=${user.return.line150} key="Total income" />
-          <${SummaryRow} keyBold=${true} currency=${true} value=${user.return.line260} key="Taxable income" />
-          <${SummaryRow} keyBold=${true} currency=${true} value=${user.return.line482} key="Total tax credits" />
+          <${SummaryRow} value=${formattedUser.line150} key="Total income" />
+          <${SummaryRow} value=${formattedUser.line260} key="Taxable income" />
+          <${SummaryRow} value=${formattedUser.line482} key="Total tax credits" />
         </dl>
 
         <${Accordion} checked=${true}>
-          <${SummaryTable} currency=${true} rows=${totalTaxRows(user.return)} />
+          <${SummaryTable} keyBold=${false} currency=${true} rows=${totalTaxRows(formattedUser)} />
         <//>
 
         <dl>
-          <${SummaryRow} keyBold=${true} currency=${true} value=${user.return.line484} key="Refund" />
+          <${SummaryRow} currency=${true} value=${formattedUser.line484} key="Refund" />
         </dl>
 
         <${Accordion} checked=${true}>
-          <${SummaryTable} currency=${true} rows=${refundRows(user.return)} />
+          <${SummaryTable} keyBold=${false} currency=${true} rows=${refundRows(formattedUser)} />
         <//>
 
         <h2 class=${inlineH2}>3.</h2>
